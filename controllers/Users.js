06afafb4548c7e25bc4d98768810e1845e40f256
Djosef0 <youssefbensaid827@gmail.com>
2022-12-25 19:38:25 +0100

    install jsonwebtoken mongoose dotenv nodemailer cors and nodemon and start to built register function

---
+++ controllers/Users.js
@@ -0,0 +1,49 @@
+import { User } from "../models/users.js";
+import { sendMail } from "../utils/sendMail.js";
+import { sendToken } from "../utils/sendToken.js";
+
+
+
+export const register  = async (req , res) =>{
+    try{
+const {name,
+       email,
+       password} = req.body;
+const {avatar} = req.files;
+
+let user  = await User.findOne({email});
+if(user){
+    return res
+    .status(200)
+    .json({
+        success : false ,
+        message : "User already Exists"});
+}
+
+const otp = Math.floor(Math.random() * 100000)
+
+user = await User.create({name ,
+     email ,
+     password ,
+     avatar ,
+     otp , 
+     otp_expiry : new Date(Date.now() + process.env.OTP_EXPIRE*60 * 1000) 
+    });
+
+await sendMail(email , 
+    "Verify your account", 
+     `Your OTP ${otp}`);
+
+
+ sendToken(
+    res ,
+    user ,
+    200 ,
+    "OTP sent to your email , Please verify your Account")
+    
+}catch(error){
+        res.status(500).json({
+            success : false ,
+            message : error.message});
+    }
+}
